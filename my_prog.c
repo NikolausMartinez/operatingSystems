cc337c1eb096cafa70d0c4d59310ec615c7bec8e
Nik Martinez <nikolaus.c.martinez@gmail.com>
2017-09-25 13:33:28 -0500

    added wait() implented perror

--- my_prog.c
+++ my_prog.c
@@ -9,25 +9,27 @@
 int main (int argc, char *argv[]) {
 	pid_t childpid = 0;
 	int i, n, opt;	
-
+	int time = 2;	
+	
+	while ((n = getopt(argc, argv, "")) != -1) {
+                switch (opt) {
+                default:
+                    perror("my_prog: Error ");
+		    exit(EXIT_FAILURE);
+                    break;
+                }
+	}
+        
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s processes\n", argv[0]);
 		return 1;
 	}
+
 	n = atoi(argv[1]);
-	while ((opt = getopt(argc, argv, "a")) != -1) {
-		switch (opt) {
-		case 'a':
-		    fprintf(stderr, "Child failed uh oh");
-		    break;
-		default:
-		    perror("Please enter a positive number");
-		    break;
-		} 
-		for (i = 1; i < n; i++)
-			if(childpid = fork())
-				break;
-		fprintf(stderr, "i:%d process ID:%1d parent ID:%1d child ID:%1d\n", i, (long)getpid(), (long)getppid(), (long)childpid);
-		return 0;
-	}
+	for (i = 1; i < n; i++)   
+            if((childpid = fork()) <=0)
+		break;
+            while(wait(NULL) > 2);    
+            fprintf(stderr, "i:%d process ID:%1d parent ID:%1d child ID:%1d\n", i, (long)getpid(), (long)getppid(), (long)childpid);
+            return 0;
 }
